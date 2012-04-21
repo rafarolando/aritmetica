121a5a7a5c02082d84304340e5025e5f0e0240a8
rafa_rolando <rafa_rolando@hotmail.com>
2012-04-21 15:41:35 -0400

    tarea

---
+++ aritmetica/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using aritmetica.Models;
+
+namespace aritmetica.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Suma()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Suma(valores val) 
+        {
+            int resul = val.valor1 + val.valor2;
+            ViewBag.respuesta = resul;
+            return View();
+        }
+        public ActionResult Resta() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Resta(valores val) 
+        {
+            int resul = val.valor1 - val.valor2;
+            ViewBag.respuesta = resul;
+            return View();
+        }
+        public ActionResult Multiplicacion() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Multiplicacion(valores val)
+        {
+            int resul = val.valor1 * val.valor2;
+            ViewBag.respuesta = resul;
+            return View();
+        }
+        public ActionResult Division() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Division(valores val)
+        {
+            string mensaje = "NO SE PUEDE DIVIDIR ENTRE 0";
+            if (val.valor2 != 0)
+            {
+                int resul = val.valor1 / val.valor2;
+                ViewBag.respuesta = resul;
+                return View();
+            }
+            else
+                
+                ViewBag.respuesta = mensaje;
+                return View();
+        }
+    }
+}
---
+++ aritmetica/Models/valores.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace aritmetica.Models
+{
+    public class valores
+    {
+        [Required]
+        public int valor1 { set; get; }
+        [Required]
+        public int valor2 { set; get; }
+        
+    }
+}
